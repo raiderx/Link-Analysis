6483e5de50a7019930abae58fb23d004ce9b95c1
Pavel Karpukhin <karpukhin.pavel@gmail.com>
2011-08-06 21:36:06 +0400

    Names of image fields were fixed

--- link-analysis-model/src/main/java/org/linkAnalysis/model/entity/Image.java
+++ link-analysis-model/src/main/java/org/linkAnalysis/model/entity/Image.java
@@ -10,23 +10,43 @@ public class Image extends DomainEntity {
     private Boolean downloaded;
     private String mimeType;
     private Integer size;
-    private Integer Height;
-    private Integer Width;
+    private Integer height;
+    private Integer width;
     private String name;
     private String extension;
 
+    /**
+     * Returns the link of the image
+     *
+     * @return the link of the image
+     */
     public Link getLink() {
         return link;
     }
 
+    /**
+     * Sets the link of the image
+     *
+     * @param link
+     */
     public void setLink(Link link) {
         this.link = link;
     }
 
+    /**
+     * Returns url of the image
+     *
+     * @return url of the image
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Sets url of the image
+     *
+     * @param url
+     */
     public void setUrl(String url) {
         this.url = url;
     }
--- link-analysis-model/src/main/java/org/linkAnalysis/model/entity/Link.java
+++ link-analysis-model/src/main/java/org/linkAnalysis/model/entity/Link.java
@@ -7,10 +7,20 @@ public class Link extends DomainEntity {
 
     private String url;
 
+    /**
+     * Returns the url of the link
+     *
+     * @return the url of the link
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Sets the url of the link
+     *
+     * @param url  link url
+     */
     public void setUrl(String url) {
         this.url = url;
     }
