825a1693516642d8c40566f75d8e14cf7e6e38e4
Pavel Karpukhin <karpukhin.pavel@gmail.com>
2011-08-08 17:05:02 +0400

    Link list view was updated Link add view was added Welcome page was added

--- link-analysis-web/src/main/java/org/linkAnalysis/web/controller/LinkController.java
+++ link-analysis-web/src/main/java/org/linkAnalysis/web/controller/LinkController.java
@@ -16,14 +16,29 @@ public class LinkController {
 
     private LinkService linkService;
 
+    /**
+     * Constructs MVC controller with objects injected via autowiring
+     *
+     * @param linkService an obejct taht provides actions on {@link Link} entity
+     */
     @Autowired
     public LinkController(LinkService linkService) {
         this.linkService = linkService;
     }
 
-    @RequestMapping(value = "/main", method = RequestMethod.GET)
-    public ModelAndView linkList() {
-        LinkSearchCriteria searchCriteria = new LinkSearchCriteria();
+    /**
+     * Handles GET request and produces page with all links
+     *
+     * @param searchCriteria search criteria
+     * @return {@link ModelAndView} with view name as linkList
+     */
+    @RequestMapping(value = "/link/list", method = RequestMethod.GET)
+    public ModelAndView list(LinkSearchCriteria searchCriteria) {
         return new ModelAndView("linkList", "linkList", linkService.getLinksByCriteria(searchCriteria));
+    }
+
+    @RequestMapping(value = "/link/add", method = RequestMethod.GET)
+    public ModelAndView add() {
+        return new ModelAndView("linkAdd");
     }
 }
---
+++ link-analysis-web/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%--
+    author: Pavel Karpukhin
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<c:redirect url="/link/list.html" />
